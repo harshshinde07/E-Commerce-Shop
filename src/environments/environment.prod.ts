212e5866082052e56fc62203d62d915ca3dba2bb
Harsh <harshshinde07@gmail.com>
2020-07-09 11:15:54 +0530

    Protecting routes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBb5CfM6rZmP39w_pC7FYaH_ofTeALKQwI",
+    authDomain: "e-commerce-shop-b3d7b.firebaseapp.com",
+    databaseURL: "https://e-commerce-shop-b3d7b.firebaseio.com",
+    projectId: "e-commerce-shop-b3d7b",
+    storageBucket: "e-commerce-shop-b3d7b.appspot.com",
+    messagingSenderId: "529727261392",
+    appId: "1:529727261392:web:95890689438f0ccd7bacd5",
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBb5CfM6rZmP39w_pC7FYaH_ofTeALKQwI",
+    authDomain: "e-commerce-shop-b3d7b.firebaseapp.com",
+    databaseURL: "https://e-commerce-shop-b3d7b.firebaseio.com",
+    projectId: "e-commerce-shop-b3d7b",
+    storageBucket: "e-commerce-shop-b3d7b.appspot.com",
+    messagingSenderId: "529727261392",
+    appId: "1:529727261392:web:95890689438f0ccd7bacd5",
+  },
 };
 
 /*
